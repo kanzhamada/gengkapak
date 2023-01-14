09e5a2d2af4df3cd2261b09585d97563aa009dcf
kanzhamada <122347083+kanzhamada@users.noreply.github.com>
2023-01-14 07:55:15 +0700

    Update finalproject.cpp

--- finalproject.cpp
+++ finalproject.cpp
@@ -1,131 +1,119 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 /* */
-int idx = 0;
+int idx;
+
 FILE *JL;
 FILE *NL;
 FILE *History;
+FILE *IStok;
+
+struct iniStok{
+	char namastok[25];
+	int qty;
+}iStok[100];
 
 struct jenis_layanan{
 	int id_jenis;
 	char jenislay[30];
-	long long int hargalay;	
+	float hargalay;	
+	int stok;
 } menujenis[100];
 
 struct nama_layanan{
 	int id_nama;
 	char nama[30];
 	char isi[100];
-	long long int hargabundling;	
+	float hargabundling;	
 } bundling[100];
 
-//struct riwayat{
-//	int tanggal, bulan, tahun;
-//	char nama;
-//	char paket;
-//	struct nama_layanan.nama paket;
-//	struct nama_layanan.hargabundling harga_paket;
-//	struct jenis_layanan.jenislay custom;
-//	struct jenis_layanan.hargalay harga_custom;
-//	int total;
-//} riwayat_pelanggan[100];
+struct entry_pesanan{
+	int tanggal, bulan, tahun;
+	char customer_name[25];
+	char pesanan[30];
+	float total_harga;
+} masukin_pesanan[100];
 
-
-
+// Prototype ------------------------
 void TampilServiceJ();
 void TampilServiceN();
+void TampilStok();
 void copyrightGengKapak();
 void EntryPesanan();
+void EntryPaket();
+void historyPesanan(int);
+int pembayaran(int , int);
+int hitungTotal (int, int);
+void wegotHistory();
+// ----------------------------------
 
 
 
 int main(){
 	int opsi;
+	
 	printf("=============================================================\n");
 	printf("===================== Barbershop Service ====================\n");
 	printf("=============================================================\n\n");
 	
-	printf("\n(1). Masukan Pesanan Pelanggan\n");
-	printf("(2). Riwayat Penjualan\n");
-	printf("\n\nPilih Opsi [1/2] : ");
+	printf("\n[1]. Masukan Pesanan Pelanggan\n");
+	printf("[2]. Riwayat Penjualan\n");
+	printf("[3]. Lihat Stok Persediaan (Tersorting)\n");
+	printf("\n\nPilih Opsi [1/2/3] : ");
 	scanf("%d", &opsi);
+	
 	if(opsi == 1){
 		copyrightGengKapak();
-		printf("\n\n     <<<<< Service Biasa >>>>>\n\n");
-		TampilServiceJ();
-		printf("\n     <<<<< Service Bundling >>>>>\n");
-		TampilServiceN();
+		system("cls");
+		
+		
 		printf("\n");
 		EntryPesanan();
 	}
 	if(opsi == 2){
-		printf("tes 2");
+		system("cls");
+		wegotHistory();
+	}
+	if(opsi == 3){
+		system("cls");
+		printf("Data telah disorting berdasarkan Stok paling sedikit\n\n");
+    	
+		TampilStok();
 	}
 	copyrightGengKapak();
 }
 
-//void InputJenisLayananBaru (){
-//	printf("\n----------------------------------\n");
-//	JL = fopen("jenislayanan.txt", "r+");
-//	if(JL == NULL){
-//		printf("File Tidak Ada!!");
-//	}
-////	struct jenis_layanan layanan;	
-//	printf("Masukan ID : "); 
-//	scanf("%d", &menujenis[idx].id_jenis);
-//	getchar();
-////	mengecek apakah ada ID yang sama?
-////	for(int i = 0; i <= 100; i++){
-////		if(menujenis[idx].id_jenis == menujenis[i].id_jenis){
-////			menujenis[i].id_jenis = '\0';
-////			printf("ID Sudah ada");
-////		}
-////	}
-//	printf("Jenis Layanan : "); 
-//	scanf("%[^\n]", menujenis[idx].jenislay);
-//	getchar();
-//	printf("Harga : "); 
-//	scanf("%lld", &menujenis[idx].hargalay);
-//	getchar();
-//	fprintf(JL, "%d - %s - %d", menujenis[idx].id_jenis, menujenis[idx].jenislay, menujenis[idx].hargalay);
-//	printf("\nId: %d || Jenis : %s || Harga : Rp.%lld, Berhasil disimpan", menujenis[idx].id_jenis, menujenis[idx].jenislay, menujenis[idx].hargalay);	
-//	idx++;
-//}
 
-//void InputNamaLayanan (){
-//	char namalay[30];
-//	int id_jenis;
-//	int total;
-//}
 
-int HitungTotalPembayaran (){
-	
+float hitungTotal (int jumlah_custom, int arr[]){
+	float total = 0;
+	int set = 0;
+	while(set < jumlah_custom){
+		for(int i = 0; i < (sizeof(menujenis)/sizeof(menujenis[0])); i++){
+			if(arr[set] == menujenis[i].id_jenis){
+				total += menujenis[i].hargalay;
+				set++;
+			}
+		}
+	}
+	return total;	
 }
 
 void Sorting(){
 	
 }
 
-int pembayaran(int idygmana, int cuan){
-	int kembali;
-	for(int i = 0; i < 100; i++){
-			if(idygmana == bundling[i].id_nama){
-				kembali = cuan - bundling[i].hargabundling;
-				return kembali;
-			} 
-		}
-	
-}
-
 void tampilkan_pencarian(int cariwoi){
 	for(int i = 0; i < 100; i++){
 			if(cariwoi == bundling[i].id_nama){
-				printf("\nPaket \t: %s\nLayanan : %s\nTotal \t: %d", bundling[i].nama, bundling[i].isi, bundling[i].hargabundling);
+				printf("\nPaket \t: %s\nLayanan : %s\n===== Total : Rp.%.3f ===== \n", bundling[i].nama, bundling[i].isi, bundling[i].hargabundling);
 			} 
 		}
 }
 
 int cari_bundling(int nyari_id){
-		for(int i = 0; i < 100; i++){
+		for(int i = 0; i < 10; i++){
 			if(nyari_id == bundling[i].id_nama){
 				return nyari_id;
 			} 
@@ -138,123 +126,141 @@ void carilagi(){
 }
 
 void EntryPesanan(){
-	int tanggal, bulan, tahun;
 	long int uang;
 	int pilih_paket;
-	char customer_name[25];
+	int milih;
 	
 	History = fopen("riwayatpesanan.txt", "r+");
 	
 	printf("---------------------- Enter Customer Data ---------------------\n");
-	printf("Transaction date (dd/mm/yy) : ");
-	scanf("%d/%d/%d", &tanggal, &bulan, &tahun);
+	printf("\nTransaction date (dd/mm/yy) : ");
+	scanf("%d/%d/%d", &masukin_pesanan[idx].tanggal, &masukin_pesanan[idx].bulan, &masukin_pesanan[idx].tahun);
 	getchar();
-	while((tanggal > 31 || tanggal < 1) || (bulan > 12 || bulan < 1) || (tahun < 2020)){
-		tanggal = 0, bulan = 0, tahun = 0;
-		printf("Tanggal Tidak Valid\n");
+	
+	while((masukin_pesanan[idx].tanggal > 31 || masukin_pesanan[idx].tanggal < 1) || (masukin_pesanan[idx].bulan > 12 || masukin_pesanan[idx].bulan < 1) || (masukin_pesanan[idx].tahun < 2020)){
+		masukin_pesanan[idx].tanggal = 0, masukin_pesanan[idx].bulan = 0, masukin_pesanan[idx].tahun = 0;
+		printf("\nTanggal Tidak Valid\n");
 		printf("Transaction date (dd/mm/yy) : ");
-		scanf("%d/%d/%d", &tanggal, &bulan, &tahun);
+		scanf("%d/%d/%d", &masukin_pesanan[idx].tanggal, &masukin_pesanan[idx].bulan, &masukin_pesanan[idx].tahun);
 		getchar();
 	}
-	printf("\nCustomer name : ");
-	scanf("%[^\n]", &customer_name);
+	
+	printf("\nCustomer name \t: ");
+	scanf("%[^\n]", &masukin_pesanan[idx].customer_name);
 	getchar();
-	printf("%s, Tanggal : %d - %d - %d", customer_name, tanggal, bulan, tahun);
-	int milih;
+	printf("\nCustomer : %s, Tanggal : %d - %d - %d\n", masukin_pesanan[idx].customer_name, masukin_pesanan[idx].tanggal, masukin_pesanan[idx].bulan, masukin_pesanan[idx].tahun);
+	
 	printf("\n(1) Bundle or (2) Custom : ");
 	scanf("%d", &milih);
+	getchar();
 	if(milih == 1){
-		printf("Pilih Id Paket : ");
-		scanf("%d", &pilih_paket);
-		getchar();
-		
-		cari_bundling(pilih_paket);
-		
-		while(cari_bundling(pilih_paket) == -1){
-			printf("\nPaket / ID Tidak Ditemukan!\n");
-			printf("Pilih Id Paket : ");
-			scanf("%d", &pilih_paket);
-			getchar();
-			cari_bundling(pilih_paket);
-		}
-		
-		tampilkan_pencarian(cari_bundling(pilih_paket));
-		printf("\nUang Pelanggan : Rp.");
-		scanf("%ld", &uang);
-		
-		printf("Kembali = Rp.%d", pembayaran(cari_bundling(pilih_paket), uang));
-		int ulang = 0;
-//		do{
-			fprintf(History, "%d/%d/%d - %s - Paket : %s (Rp.%d) - Bayar : Rp.%d, Kembali Rp.%d\n", tanggal, bulan, tahun, customer_name, bundling[cari_bundling(pilih_paket)].nama, bundling[cari_bundling(pilih_paket)].hargabundling, uang, pembayaran(cari_bundling(pilih_paket), uang));
-			++ulang;
-//		} while(ulang <= 100);
-			
+		system("cls");
+		printf("     <<<<< Service Paket/Bundling >>>>>\n\n");
+		TampilServiceN();
+		EntryPaket();
+	
 		} else if (milih == 2){
+		system("cls");	
+		printf("     <<<<< Service Biasa/Custom >>>>>\n");
 		TampilServiceJ();
 		int jumlah_custom;
-		printf("Jumlah yang akah dipilih : ");
+		copyrightGengKapak();
+		printf("\n\nJumlah yang akah dipilih : ");
 		scanf("%d", &jumlah_custom);
+		getchar();
 		int arr[jumlah_custom];
 		
 		for(int i = 0; i < jumlah_custom; i++){
-			printf("\nPilih Layanan : ");
+			printf("\nPilih (Id) Layanan Ke-[%d] dari %d : ", i+1, jumlah_custom);
 			scanf("%d", &arr[i]);
+			getchar();
 			for(int j = 0; j < 15; j++){
 				if(arr[i] == menujenis[j].id_jenis){
-					printf("\n%s, Rp.%d\n", menujenis[j].jenislay, menujenis[j].hargalay);
+					printf("________________________________\n%d. %s, Rp.%.3f\n________________________________", i+1, menujenis[j].jenislay, menujenis[j].hargalay);
 				}
 			}
 		}
-		int total = 0;
-		int set = 0;
-		while(set < jumlah_custom){
-			for(int i = 0; i < 15; i++){
-				if(arr[set] == menujenis[i].id_jenis){
-					total += menujenis[i].hargalay;
-					set++;
-				}
-				
-			}
-		}	
-		printf("\nTotal : %d", total);	
-//		printf("\nPilih Layanan : ");
-//		for(int i = 0; i < jumlah_custom-1; i++){
-//			scanf("%d", &arr[i]);
-//		}
-//		for(int i = 0; i < jumlah_custom; i++){
-//			int total;
-//			for(int j = 0; j < 15; j++){
-//				if(arr[i] == menujenis[j].id_jenis){
-//					total += menujenis[j].hargalay;
-//					printf("Total %d", total);
-//				}
-//			}
-//			
-//		}
+		printf("\n============ Total : Rp.%.3f =================\n\n", hitungTotal(jumlah_custom, arr));
+		printf("Pembelian Berhasil :))");
 		
 	}
 }
 
 void copyrightGengKapak(){
-	printf("\n---------------- Copyright by @Geng Kapak 2023 ---------------");
+	printf("\n---------------- Copyright by @Geng Kapak 2023 ---------------\n");
 }
 
-void RiwayatPesanan(){
+void EntryPaket(){
+	int pilih_paket, uang;
+	printf("\nPilih Id Paket : ");
+	scanf("%d", &pilih_paket);
+	getchar();
+		
+	cari_bundling(pilih_paket);
+		
+	while(cari_bundling(pilih_paket) == -1){
+		printf("\nPaket / ID Tidak Ditemukan!\n");
+		printf("\nPilih Id Paket : ");
+		scanf("%d", &pilih_paket);
+		getchar();
+		cari_bundling(pilih_paket);
+	}
+	tampilkan_pencarian(cari_bundling(pilih_paket));
+	historyPesanan(pilih_paket);
+}
+
+void historyPesanan(int id){
+	History = fopen("riwayatpesanan.txt", "+");
+	for(int i = 0; i < (sizeof(bundling)/sizeof(bundling[0])); i++){
+		if(id == bundling[i].id_nama){
+			strcpy(masukin_pesanan[idx].pesanan, bundling[i].nama);
+			masukin_pesanan[idx].total_harga = bundling[i].hargabundling;
+		}
+	}
+	for(int i = 0; i <= idx; i++){
+			fprintf(History, "%d/%d/%d - Atas Nama : %s, Pesanan : %s, Total : Rp.%.3f\n",
+	masukin_pesanan[idx].tanggal, masukin_pesanan[idx].bulan, masukin_pesanan[idx].tahun,
+	masukin_pesanan[idx].customer_name, masukin_pesanan[idx].pesanan, masukin_pesanan[idx].total_harga);
+	
+	}
+
+//	fclose(History);	
+		
+}
+
+void wegotHistory(){
+	History = fopen("riwayatpesanan.txt", "r+");
+	
+	while(fscanf(History, "%d/%d/%d - Atas Nama : %[^,], Pesanan : %[^,], Total : Rp.%f\r\n",&masukin_pesanan[idx].tanggal, &masukin_pesanan[idx].bulan, &masukin_pesanan[idx].tahun,&masukin_pesanan[idx].customer_name, &masukin_pesanan[idx].pesanan, &masukin_pesanan[idx].total_harga) != EOF){
+		
+		printf("%d/%d/%d - Atas Nama : %s, Pesanan : %s, Total : Rp.%.3f\n",
+	masukin_pesanan[idx].tanggal, masukin_pesanan[idx].bulan, masukin_pesanan[idx].tahun,
+	masukin_pesanan[idx].customer_name, masukin_pesanan[idx].pesanan, masukin_pesanan[idx].total_harga);
+	idx++;
+	}
+}
+
+void TampilStok(){
+	IStok = fopen("stok.txt", "r+");
+		while (fscanf(IStok, "%[^|]|%d\r\n", &iStok[idx].namastok, &iStok[idx].qty) != EOF){
+			printf("Qty : %d,\t %s\n", iStok[idx].qty, iStok[idx].namastok);
+			idx++;
+		}
 	
 }
 
 void TampilServiceJ(){
 	JL = fopen("jenislayanan.txt", "r+");
-	while (fscanf(JL, "%d-%[^-]-%d\r\n", &menujenis[idx].id_jenis, &menujenis[idx].jenislay, &menujenis[idx].hargalay) != EOF){
-		printf("________________________________\nId \t : %d\nLayanan\t : %s \nHarga \t : Rp.%d\n", menujenis[idx].id_jenis, menujenis[idx].jenislay, menujenis[idx].hargalay);
+	while (fscanf(JL, "%d|%[^|]|%f\r\n", &menujenis[idx].id_jenis, &menujenis[idx].jenislay, &menujenis[idx].hargalay) != EOF){
+		printf("________________________________\nId \t : %d\nLayanan\t : %s\nHarga \t : Rp.%.3f\n", menujenis[idx].id_jenis, menujenis[idx].jenislay, menujenis[idx].hargalay);
 		idx++;
 	}
 }
 
 void TampilServiceN(){
 	NL = fopen("namalayanan.txt", "r+");
-	while (fscanf(NL, "%d-%[^-]-%[^-]-%d\r\n", &bundling[idx].id_nama, &bundling[idx].nama, &bundling[idx].isi, &bundling[idx].hargabundling) != EOF){
-		printf("________________________________\nId \t : %d\nPaket\t : %s \nLayanan\t : %s \nHarga \t : Rp.%d\n", bundling[idx].id_nama, bundling[idx].nama, bundling[idx].isi, bundling[idx].hargabundling);
+	while (fscanf(NL, "%d-%[^-]-%[^-]-%f\r\n", &bundling[idx].id_nama, &bundling[idx].nama, &bundling[idx].isi, &bundling[idx].hargabundling) != EOF){
+		printf("________________________________\nId \t : %d\nPaket\t : %s \nLayanan\t : %s \nHarga \t : Rp.%.3f\n", bundling[idx].id_nama, bundling[idx].nama, bundling[idx].isi, bundling[idx].hargabundling);
 		idx++;
 	}
 }
